70d6a851b1d3127149bf7128ebc217f0944e77ff
gabriela <hwgaby87@gmail.com>
2026-04-27 22:43:53 -0300

    Documentación y normalización de nombres y estructura

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './global.css'
 createRoot(document.getElementById('root')).render(
   // BrowserRouter permite que funcionen las rutas (URLs) en la app
   <BrowserRouter>
-    // AuthContextProvider envuelve la app para que todos los componentes sepan si el usuario está logueado
+    {/* AuthContextProvider envuelve la app para que todos los componentes sepan si el usuario está logueado */}
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
